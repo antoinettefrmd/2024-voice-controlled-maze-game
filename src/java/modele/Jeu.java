95b02688be33dffd766cfb3c01e281091196a412
Léa Benoiton <benlem.lea@gmail.com>
2023-03-07 11:29:08 -0100

    jeu

--- src/java/modele/Jeu.java
+++ src/java/modele/Jeu.java
@@ -5,8 +5,14 @@ import vue.LabyrinthGraphique;
 public class Jeu {
 	
 	public static LabyrinthGraphique labyrinth;
+	public static ListeDeJoueurs joueurs;
+	public static ListeDeJoueurs joueursfinito;
+	public static Joueur courant;
+	public int etage;
 	
-	
+	public Jeu() {
+		
+	}
 	
 	public void jouer() {
 		
