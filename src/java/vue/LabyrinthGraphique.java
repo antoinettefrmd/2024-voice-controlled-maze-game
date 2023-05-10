ed112d69cf596d6c52ad764eca39f023f69d297e
Léa Benoiton <benlem.lea@gmail.com>
2023-05-10 22:18:03 +0000

    enlèvement clavier

--- src/java/vue/LabyrinthGraphique.java
+++ src/java/vue/LabyrinthGraphique.java
@@ -39,14 +39,7 @@ public class LabyrinthGraphique extends JPanel{
 	public LabyrinthGraphique(int n, ListeDeJoueurs p, boolean b) {
 		
 		this.labyrinthD = new Labyrinth(n, p, b);
-		this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), "right");
-		this.getActionMap().put("right", right);
-		this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), "left");
-		this.getActionMap().put("left", left);
-		this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), "up");
-		this.getActionMap().put("up", up);
-		this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), "down");
-		this.getActionMap().put("down", down);
+		
 		setLayout(new GridLayout(2*n+1, 2*n+1));
 		labyrinthG = new JPanel[2*n +1][2*n +1];
 		
@@ -76,46 +69,7 @@ public class LabyrinthGraphique extends JPanel{
 		requestFocus();
 		
 	}
-	
-	
-	public void placerJoueurs(ListeDeJoueurs joueurs) {
-		
-	}
-	private Action right = new AbstractAction() {
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			LabyrinthGraphique.this.labyrinthD.droite(LabyrinthGraphique.this.labyrinthD.getCurrent().getJoueur());
-			repaint();
-		}
-	};
 
-	private Action left = new AbstractAction() {
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			LabyrinthGraphique.this.labyrinthD.gauche(LabyrinthGraphique.this.labyrinthD.getCurrent().getJoueur());
-			repaint();
-		}
-	};
-	
-	private Action up = new AbstractAction() {
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			LabyrinthGraphique.this.labyrinthD.haut(LabyrinthGraphique.this.labyrinthD.getCurrent().getJoueur());
-			repaint();
-		}
-	};
-	
-	private Action down = new AbstractAction() {
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			LabyrinthGraphique.this.labyrinthD.bas(LabyrinthGraphique.this.labyrinthD.getCurrent().getJoueur());
-			repaint();
-		}
-	};
 	public Labyrinth getLabyrinthD() {
 		return labyrinthD;
 	}
